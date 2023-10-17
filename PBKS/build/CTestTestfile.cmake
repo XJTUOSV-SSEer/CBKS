e65a2f93aa2f009ccaea214b34d6a8442e9d4fa8
chuansao825 <1641902829@qq.com>
2023-10-17 17:02:19 +0800

    clear_build

--- PBKS/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhg/code/CBKS/PBKS
-# Build directory: /home/zhg/code/CBKS/PBKS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
